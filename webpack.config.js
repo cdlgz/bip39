3dc69d15df721cdb27a4b2d119a7a1531e79689f
admin <deng.zz@qq.com>
2018-10-15 19:48:06 +0800

    fix webpack bug

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,23 @@
 const path = require('path');
-const webpack = require('webpack');
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 
 module.exports = {
-   mode: "production",
-   entry: {
-      entry: __dirname + '/src/hdwsdk.js'
-   },
-   output: {
-      path: path.resolve(__dirname, './dist'),
-      filename: 'hdwsdk.min.js',
-   },
-   plugins: [
-      new UglifyJsPlugin()
-   ],
-   devtool: "source-map"
+  mode: "production",
+  entry: {
+    entry: __dirname + '/src/hdwsdk.js'
+  },
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    filename: 'hdwsdk.min.js',
+  },
+  optimization: {
+    minimizer: [
+      new UglifyJsPlugin({
+        uglifyOptions: {
+          keep_fnames: true
+        }
+      })
+    ]
+  },
+  devtool: "source-map"
 }
