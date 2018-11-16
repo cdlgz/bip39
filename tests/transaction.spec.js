e64f41b38bd1a48b7dfec0c8cace95a700549a47
zhizhong <deng.zhizhong@shuweikeji.com>
2018-11-16 18:12:02 +0800

    add transaction spec file

---
+++ tests/transaction.spec.js
@@ -0,0 +1,12 @@
+var assert = require('assert');
+var HDWJS = require('../src/hdwsdk');
+
+describe('Transaction', function () {
+
+  describe('#createTransaction()', function () {
+
+    //TODO:
+
+  });
+
+});
