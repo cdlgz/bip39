798f0884be0eba657a0d1448f3029f3f2d62beb5
eosaction <deng.zz@qq.com>
2019-03-21 15:35:09 +0800

    update vhkd int53

---
+++ fix.sh
@@ -0,0 +1,3 @@
+cp ./int53/* ./node_modules/int53/ -rf
+cp ./fix_modules/typeforce/* ./node_modules/typeforce/ -rf
+cp ./fix_modules/bitcoinjs-lib/* ./node_modules/bitcoinjs-lib/ -rf
--- tests/transaction.vhkd.spec.js
+++ tests/transaction.vhkd.spec.js
@@ -107,7 +107,7 @@ describe('VHKD Transaction', function () {
       assert.equal(fee.data, 100000000000);
     });
 
-    it('should build transaction ok', function () {
+    it('should build transaction ok 1', function () {
       var coinData = {
         currency: 'vhkd',
         utxos: [{
@@ -127,6 +127,35 @@ describe('VHKD Transaction', function () {
       console.log(txHex);
       assert.equal(txHex.status, true);
       assert.equal(txHex.data, '0100000001e733614ca5f733791a78ddc85f5bb40ce74bd704a0c8cf85bd3e44a330f8c5cf000000006a47304402207c9863d395e5c68ff768d76ef9cacd1f65655b05c0450608410b5c6ac8fa333c02205c1a83da0f2094b3344a2f677c4cda7851304f60ede51e3d405208987c6dd12e0121039409b25aeda4686f8e94c0eacb90bd6c516ca0967f737d2b7b773bc53075e784ffffffff0200e1f505000000001976a914a83f766e285dd3494b03361a7a358e01df7972b288acd073a435000000001976a914b58a8a1391d9c4fd7b74e800541477eb0021d19788ac00000000');
+    });
+
+    it('should build transaction ok 2', function () {
+      var coinData = {
+        currency: 'vhkd',
+        utxos: [{
+          txId: '321be957fd57b6b5194f35686a6a760f444065c5dba4de16db2a1da8c97fbc60',
+          vout: 1,
+          value: 123400000000,
+          key: 'Ky4j9NukFVWzmo2Z4GXhPmbd9cfkLCUn9uvok1LmcWq21KdxLuPo'
+        },{
+          txId: 'd9eeb7fcfe49e7a5cbf6daeaf6e303dde0179ecdeeac34ca521f800dba5e0914',
+          vout: 1,
+          value: 710849032353190400,
+          key: 'L15B5jyqpWRzCZH2LkGT4S9YPnYGmyXFXjYnSjPN8z3j58gDsBH7'
+        }],
+        targets: [{
+          address: 'VTHkm491W3uvvSnYhQG5RkWSW9dnbr11sQ',
+          value: 10000000000
+        }],
+        feeRate: 3.0E-4,
+        maxFee: 100000000000,
+        minFee: 30000,
+        changeAddress: 'VGuVZ8LNoW6idXGkDS7dAVB2Ftawqy5yzz'
+      };
+      var txHex = BCOINJS.bcoin.buildTransaction(coinData);
+      console.log(txHex);
+      assert.equal(txHex.status, true);
+      assert.equal(txHex.data, '010000000114095eba0d801f52ca34aceecd9e17e0dd03e3f6eadaf6cba5e749fefcb7eed9010000006a473044022058d9610fc36460108b9a0a8f8b7a2ebb59cad995d34d4d5b21f90e600b92b1b502204600089bba277a04505cc0477f8377826dbb4159de3a239e99b85af87a5b890d0121020bf8a04fd0338960bb2bb9794e6c5f89e7c78c26c55aa8fbdc57f20380d46f2bffffffff0200e40b54020000001976a914b66aba2d8acc077deda90f9ea284b88f22823e1188ac00e71d6a6b71dd091976a9144483a00ea5ff1fbbfc362f258b7df73d11d1e79488ac00000000');
     });
 
     it('should build transaction ok with fully transfer', function () {
